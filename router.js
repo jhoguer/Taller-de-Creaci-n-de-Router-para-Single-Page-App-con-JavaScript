e0108d229ddcd0674c3f878fc13f2472687218b3
Jhon Guerrero <jhoguer@gmail.com>
2020-02-03 22:15:36 -0500

    Creando nuestro archivo de rutas

---
+++ router.js
@@ -0,0 +1,14 @@
+class Router {
+
+  constructor(routes) {
+    this.routes = routes;
+    this._loadInitialRoute();
+  }
+  
+  _loadInitialRoute() {
+    const pathNameSplit = window.location.pathname.split('/');
+    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit : '';
+  
+    this.loadRoute(...pathSegs)
+  }
+}
